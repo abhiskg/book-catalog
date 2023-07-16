6c2732e46acb6db120dd5f309bc244732c28ab9f
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-07-16 16:16:40 +0600

    Footer added

--- frontend/src/layout/components/SiteFooter.tsx
+++ frontend/src/layout/components/SiteFooter.tsx
@@ -1,5 +1,43 @@
+import { Container, Group, createStyles, rem } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: rem(120),
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan("xs")]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}));
+
 function SiteFooter() {
-  return <div>SiteFooter</div>;
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        Book Catalog
+        <Group className={classes.links}>Hi</Group>
+      </Container>
+    </div>
+  );
 }
 
 export default SiteFooter;
