ca7bdeb1ae1075a30e17c0c3fbc8a8c6b85e4b4a
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-07-16 17:38:48 +0600

    book card added

---
+++ frontend/src/components/card/BookCard.tsx
@@ -0,0 +1,120 @@
+import {
+  ActionIcon,
+  Badge,
+  Button,
+  Card,
+  Group,
+  Image,
+  Text,
+  createStyles,
+  rem,
+} from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+  },
+
+  section: {
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    paddingBottom: theme.spacing.md,
+  },
+
+  like: {
+    color: theme.colors.red[6],
+  },
+
+  label: {
+    textTransform: "uppercase",
+    fontSize: theme.fontSizes.xs,
+    fontWeight: 700,
+  },
+}));
+
+interface BadgeCardProps {
+  image: string;
+  title: string;
+  country: string;
+  description: string;
+  badges: {
+    emoji: string;
+    label: string;
+  }[];
+}
+
+export function BadgeCard({
+  image,
+  title,
+  description,
+  country,
+  badges,
+}: BadgeCardProps) {
+  const { classes, theme } = useStyles();
+
+  const features = badges.map((badge) => (
+    <Badge
+      color={theme.colorScheme === "dark" ? "dark" : "gray"}
+      key={badge.label}
+      leftSection={badge.emoji}
+    >
+      {badge.label}
+    </Badge>
+  ));
+
+  return (
+    <Card withBorder radius="md" p="md" className={classes.card}>
+      <Card.Section>
+        <Image src={image} alt={title} height={180} />
+      </Card.Section>
+
+      <Card.Section className={classes.section} mt="md">
+        <Group position="apart">
+          <Text fz="lg" fw={500}>
+            {title}
+          </Text>
+          <Badge size="sm">{country}</Badge>
+        </Group>
+        <Text fz="sm" mt="xs">
+          {description}
+        </Text>
+      </Card.Section>
+
+      <Card.Section className={classes.section}>
+        <Text mt="md" className={classes.label} c="dimmed">
+          Perfect for you, if you enjoy
+        </Text>
+        <Group spacing={7} mt={5}>
+          {features}
+        </Group>
+      </Card.Section>
+
+      <Group mt="xs">
+        <Button radius="md" style={{ flex: 1 }}>
+          Show details
+        </Button>
+        <ActionIcon variant="default" radius="md" size={36}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={classes.like}
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+          </svg>
+        </ActionIcon>
+      </Group>
+    </Card>
+  );
+}
